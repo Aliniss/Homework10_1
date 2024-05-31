064a35f597cd752f655fe45d43a7a331c2c3f7ca
Komarov Vyacheslav <vijk60001@gmail.com>
2024-06-01 02:39:58 +0300

    2V

--- src/masks.py
+++ src/masks.py
@@ -5,38 +5,26 @@ def mask_card_number(card_number: str) -> str:
     if len(card_number) < 10:
         print("Недостаточно цифр. Добавьте 10 или более цифр в номер карты.")
         return card_number
-
-    # Удаляем пробелы из номера карты
-    card_number = card_number.replace(" ", "")
-
-    # Заменяем цифры в середине на "**"
-    masked_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
-
-    return masked_number
+    if "Visa Classic" in card_number:
+        masked_number = card_number[:17] + card_number[18:20] + "** **** " + card_number[-4:]
+        return masked_number
+    elif "Visa Platinum" in card_number:
+        masked_number = card_number[:18] + card_number[20:22] + "** **** " + card_number[-4:]
+        return masked_number
+    elif "Visa Gold" in card_number:
+        masked_number = card_number[:14] +  card_number[15:17] + "** **** " + card_number[-4:]
+        return masked_number
+    elif "Maestro" in card_number:
+        masked_number = card_number[:12] + card_number[13:15] + "** **** " + card_number[-4:]
+        return masked_number
+    elif "MasterCard" in card_number:
+        masked_number = card_number[:16] + card_number[16:18] + "** **** " + card_number[-4:]
+        return masked_number
+    elif "Счет" in card_number:
+        masked_account = card_number[:4] + " " + "**" + card_number[-4:]
+        return masked_account
 
 
 input_card_number = input("Введите номер карты: ")
 masked_card_number = mask_card_number(input_card_number)
 print(masked_card_number)
-
-
-def mask_account_number(account_number: str) -> str:
-    """Принимает на вход номер счёта и возвращает маску."""
-
-    # Проверяем, что длина номера счета больше или равна 4 (все остальные цифры скрыты)
-    if len(account_number) < 4:
-        print("Недостаточно цифр. Добавьте 4 или более цифры в номер счета.")
-        return account_number
-
-    # Удаляем пробелы из номера счета
-    account_number = account_number.replace(" ", "")
-
-    # Оставляем только последние 4 цифры
-    masked_number = "**" + account_number[-4:]
-
-    return masked_number
-
-
-input_account_number = input("Введите номер счета: ")
-masked_account_number = mask_account_number(input_account_number)
-print(masked_account_number)
