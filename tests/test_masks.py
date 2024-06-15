aaf682b0ec1b173404f52362cef4d66a347e8029
Komarov Vyacheslav <vijk60001@gmail.com>
2024-06-15 13:20:08 +0300

    homework_10_2 rework_1

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,6 +3,11 @@ import pytest
 from src.masks import mask_card_number, convert_date
 
 
+@pytest.fixture
+def date():
+    return "2018-07-11T02:26:18.671407"
+
+
 @pytest.mark.parametrize("x, expected", [("Visa Platinum 7000 7922 8960 6361", "Visa Platinum 7000 79** **** 6361"), (
         "Maestro 7000 7922 8960 6361", "Maestro 7000 79** **** 6361"),
                                          ("Счет 73654108430135874305", "Счет **4305")])
@@ -10,6 +15,5 @@ def test_add(x, expected):
     assert mask_card_number(x) == expected
 
 
-@pytest.mark.parametrize("x, expected", [("2018-07-11T02:26:18.671407", "11.07.2018")])
-def teste_dated(x, expected):
-    assert convert_date(x) == expected
+def test_dated(date):
+    assert convert_date(date) == "11.07.2018"
