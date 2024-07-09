3bb0e4e86b0c47c6660163d1e048a96049280559
Komarov Vyacheslav <vijk60001@gmail.com>
2024-07-09 14:56:06 +0300

    rework files

--- src/masks.py
+++ src/masks.py
@@ -1,64 +1,27 @@
 import logging
 
 logger = logging.getLogger("masks")
-logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("logs/masks.log", mode='w')
-file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
-file_handler.setFormatter(file_formater)
+file_handler = logging.FileHandler("loggers_info.txt")
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
-def mask_card_number(card_number: str) -> str | None:
+def masked_card_num(num: str) -> str:
+    """функция принимает номер карты и возвращает замаскированный номер
+    @rtype: object
     """
-Функция принимает на вход номер кредитной карты в виде строки и возвращает строку с маскированным номером.
-
-Args:
-card_number: str - номер кредитной карты
-"""
-
-    #   Проверяем тип карты
-    if "Visa Classic" in card_number:
-        masked_number = card_number[:17] + " " + card_number[18:20] + "** **** " + card_number[-4:]
-        logger.info(f"Получаем зашифрованный номер карты {masked_number}")
-        return masked_number
-    elif "Visa Platinum" in card_number:
-        masked_number = card_number[:18] + " " + card_number[19:21] + "** **** " + card_number[-4:]
-        logger.info(f"Получаем зашифрованный номер карты {masked_number}")
-        return masked_number
-    elif "Visa Gold" in card_number:
-        masked_number = card_number[:14] + " " + card_number[15:17] + "** **** " + card_number[-4:]
-        logger.info(f"Получаем зашифрованный номер карты {masked_number}")
-        return masked_number
-    elif "Maestro" in card_number:
-        masked_number = card_number[:12] + " " + card_number[13:15] + "** **** " + card_number[-4:]
-        logger.info(f"Получаем зашифрованный номер карты {masked_number}")
-        return masked_number
-    elif "MasterCard" in card_number:
-        masked_number = card_number[:16] + " " + card_number[16:18] + "** **** " + card_number[-4:]
-        logger.info(f"Получаем зашифрованный номер карты {masked_number}")
-        return masked_number
-    elif "Maestro" in card_number:
-        masked_number = card_number[:12] + " " + card_number[13:15] + "** **** " + card_number[-4:]
-        logger.info(f"Получаем зашифрованный номер карты {masked_number}")
-        return masked_number
-    elif "Счет" in card_number:
-        masked_account = card_number[:4] + " " + "**" + card_number[-4:]
-
-        logger.info(f"Получаем зашифрованный номер карты {masked_account}")
-
-        return masked_account
-    return None
+    logger.info(f"start masked_card_num {num}")
+    number = 4
+    result = num[:number] + " " + num[number : number + 2] + "** ****" + " " + num[number + 8 :]
+    logger.info(f"mask {result}")
+    return result
 
 
-def convert_date(date_str: str) -> str:
-    """
-    Принимает на вход строку и преобразует её в дату
-
-    Args:
-    date_str (str): строка с датой
-    """
-    date_parts = date_str.split("T")[0].split("-")
-    year = date_parts[0]
-    month = date_parts[1]
-    day = date_parts[2]
-    return f"{day}.{month}.{year}"
+def masked_account_num(num: str) -> str:
+    """функция принимает номер счёта и возвращает замаскированный номер"""
+    logger.info(f"start masked_account_num {num}")
+    result = "**" + num[-4:]
+    logger.info(f"mask {result}")
+    return result
---
+++ src/widget.py
@@ -0,0 +1,28 @@
+from src.masks import masked_account_num, masked_card_num
+
+
+def mask_elements(element: str) -> str | None:
+    """Функция, маскирующая любой элемент,
+    как номер карты, так и номер счёта"""
+    if element[0] == "С":
+        for i in range(len(element)):
+            if element[i].isalpha():
+                continue
+            elif element[i].isdigit():
+                mask = masked_account_num(element[i:])
+                return f"Счет {mask}"
+
+    else:
+        for i in range(len(element)):
+            if element[i].isalpha():
+                continue
+            elif element[i].isdigit():
+                mask = masked_card_num(element[i:])
+                return f"{element[:-16]}{mask}"
+    return None
+
+
+def get_date(date: str) -> str:
+    """функция получает дату из полученных данных, и выводит её"""
+    day, month, year = date[8:10], date[5:7], date[:4]
+    return f"{day}.{month}.{year}"
